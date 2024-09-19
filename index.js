adcce53a45f6ad1f51cff9ec07348541f7edea50
uralbek-v1-0-0 <versiya.v1.0.0@gmail.com>
2024-09-19 18:17:22 +0500

    update index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("001").innerHTML = "• 001 — Veb-saytning yaratilish tarixi";
+document.getElementById("002").innerHTML = "• 002 — Birinchi CSS dizayn yaratildi.";
+document.getElementById("003").innerHTML = "• 003 — HTMLni o‘rganish bo‘limi qo‘shildi.";
+document.getElementById("izoh1").innerHTML = "Quyida barcha o‘zgarishlar va nashrlar ro‘yxati bilan batafsil tanishib chiqishingiz mumkin!";
+document.getElementById("1").innerHTML = "Versiya – v1.0.0";
+document.getElementById("izoh2").innerHTML = "Ushbu versiyaga jami 157 marta o‘zgartirish kiritilgan bo‘lib, muallif har safar yangi o‘zgarishlarni ommaviy nashr etishdan oldin eski nashrlarni serverda to‘liq saqlab qolishga harakat qilgan.";
+document.getElementById("").innerHTML = "";
